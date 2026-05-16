592243de6f6a75441541d79c984378a95800fae2
Klaudia0212 <klaudia.scigalska@gmail.com>
2026-05-16 15:27:19 +0200

    dodawanie spotkania

---
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -0,0 +1,40 @@
+import {useState} from "react";
+import NewMeetingForm from "./NewMeetingForm";
+import MeetingsList from "./MeetingsList";
+
+export default function MeetingsPage({username}) {
+    const [meetings, setMeetings] = useState([]);
+    const [addingNewMeeting, setAddingNewMeeting] = useState(false);
+
+    async function handleNewMeeting(meeting) {
+     const response = await fetch('/api/meetings', {
+         method: 'POST',
+         body: JSON.stringify(meeting),
+         headers: { 'Content-Type': 'application/json' }
+     });
+     if (response.ok) {
+         const nextMeetings = [...meetings, meeting];
+         setMeetings(nextMeetings);
+         setAddingNewMeeting(false);
+     }
+    }
+
+    function handleDeleteMeeting(meeting) {
+        const nextMeetings = meetings.filter(m => m !== meeting);
+        setMeetings(nextMeetings);
+    }
+
+    return (
+        <div>
+            <h2>Zajęcia ({meetings.length})</h2>
+            {
+                addingNewMeeting
+                    ? <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)}/>
+                    : <button onClick={() => setAddingNewMeeting(true)}>Dodaj nowe spotkanie</button>
+            }
+            {meetings.length > 0 &&
+                <MeetingsList meetings={meetings} username={username}
+                              onDelete={handleDeleteMeeting}/>}
+        </div>
+    )
+}
