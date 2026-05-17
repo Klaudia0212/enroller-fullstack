3105e20e1890ce82bedfd83e13374ff7255bff13
Klaudia0212 <klaudia.scigalska@gmail.com>
2026-05-17 13:15:47 +0200

    usuwanie spotkań

--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -31,22 +31,34 @@ export default function MeetingsPage({username}) {
      }
     }
 
-    function handleDeleteMeeting(meeting) {
-        const nextMeetings = meetings.filter(m => m !== meeting);
-        setMeetings(nextMeetings);
+ async function handleDeleteMeeting(meeting) {
+        const response = await fetch(`/api/meetings/${meeting.id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            const nextMeetings = meetings.filter(m => m.id !== meeting.id);
+            setMeetings(nextMeetings);
+        }
     }
 
     return (
         <div>
             <h2>Zajęcia ({meetings.length})</h2>
+
             {
                 addingNewMeeting
-                    ? <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)}/>
+                    ? <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)} />
                     : <button onClick={() => setAddingNewMeeting(true)}>Dodaj nowe spotkanie</button>
             }
+
             {meetings.length > 0 &&
-                <MeetingsList meetings={meetings} username={username}
-                              onDelete={handleDeleteMeeting}/>}
+                <MeetingsList
+                    meetings={meetings}
+                    username={username}
+                    onDelete={handleDeleteMeeting}
+                />
+            }
         </div>
-    )
+    );
 }
